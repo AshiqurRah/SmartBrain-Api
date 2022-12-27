9e93fa3d80eb1bf0617e17369fcbd2bfc5cbfd6e
Ashiqur Rahman <e0689695@u.nus.edu>
2022-12-28 07:14:10 +0800

    Create main route and signin route

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express')
 
-const app = express()
+const app = express();
+
+app.get('/', (req,res)=> {
+    res.send('this is working')
+})
+
+app.post('/signin', (req, res) => {
+    res.send('signing in')
+})
 
 app.listen(3000, () => {
     console.log("listening to port 3000");
 })
+
