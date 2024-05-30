50eff8049497fc097fb921a26ca5ee2da2b13429
AshiqurRah <89114596+AshiqurRah@users.noreply.github.com>
2024-05-30 10:43:44 +0800

    Update image.js

--- controllers/image.js
+++ controllers/image.js
@@ -3,14 +3,8 @@ const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
 const stub = ClarifaiStub.grpc();
 
 const metadata = new grpc.Metadata();
-metadata.set("authorization", "Key 64ad26ac6c704b758786c94d5275a175");
+metadata.set("authorization", "Key ______");
 
-// const app = new Clarifai.App({
-//     apiKey: '64ad26ac6c704b758786c94d5275a175'
-// });
-
-// const Clarifai = require('clarifai')
-// console.log(Clarifai)
 
 
 const handleImage = (req, res,db) => {
@@ -51,22 +45,6 @@ const handleAPIcall = (req,res) => {
             res.json(response)
         }
         );
-        // app.models
-        // .predict(
-        //     // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-        //     // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-        //     // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-        //     // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-        //     // is to use a different version of their model that works like the ones found here: https://github.com/Clarifai/clarifai-javascript/blob/master/src/index.js
-        //     // so you would change from:
-        //     // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-        //     // to:
-        //     // .predict('53e1df302c079b3db8a0a36033ed2d15', this.state.input)
-        //     Clarifai.FACE_DETECT_MODEL,
-        //     req.body.input).then(data => {
-        //         res.json(data)
-        //     })
-        //     .catch(err => res.status(400).json('unable to work with API'))
     }
     
     module.exports = {
